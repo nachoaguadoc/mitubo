3b88eb29c5fbb9be7a9318ca00d1d1e08265a0d0
nachoaguadoc <nachoaguadoc@gmail.com>
2014-12-21 15:36:58 +0100

    Added init script

---
+++ runMitubo.sh
@@ -0,0 +1,10 @@
+#1/bin/bash
+#Arranca MongoDB
+
+echo "Arrancando MongoDB"
+sudo mongod --fork --logpath /var/log/mongod.log
+
+#Corre la app de node
+echo "Arrancando MiTubo con forever"
+sudo forever start app.js
+
